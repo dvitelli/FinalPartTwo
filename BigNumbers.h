a9a1fbed189f5ce809b0ca48f6c064ffb332b623
Dom <46989387+dvitelli@users.noreply.github.com>
2019-05-14 23:42:14 -0700

    Update BigNumbers.h

--- BigNumbers.h
+++ BigNumbers.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "stdafx.h""
+#include "stdafx.h"
 #include <vector>
 #include <iostream>
 
@@ -303,32 +303,181 @@ namespace BigNumbers
 				return newVector;
 
 
-			}
-			std::vector<int>& operator *(MyBigNumbers vectorTwo) {}
+			} /*
+			std::vector<int>& operator *(MyBigNumbers vectorTwo) {
 
-			/*	std::vector<int> newVector;
+                std::vector<int> newVector, firstVector;
 				int multiplication, carry;
 				bool tenPlus = false;
 				int multiplicationCounter;
+				int addition;
 
 				auto it = bigVector.rbegin();
 				auto itTwo = vectorTwo.bigVector.rbegin();
 
+				auto itThree = newVector.rbegin();
+				auto itFour = firstVector.rbegin();
+
 				if (bigVector.size() >= vectorTwo.bigVector.size()) {
 
-					for (itTwo; itTwo != vectorTwo.bigVector.rend(); itTwo++) {
+					for (itTwo; itTwo != vectorTwo.bigVector.rend(); itTwo++, multiplicationCounter++) {
+
+						if (newVector.size() >= firstVector.size()) {
+
+							for (itFour; itFour != firstVector.rend(); itThree++, itFour++) {
+
+								addition = *itThree + *itFour;
+
+								if (tenPlus == true) {
+
+									addition += 1;
+									tenPlus = false;
+								}
+
+								if (addition >= 10) {
+
+									tenPlus = true;
+									addition -= 10;
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+								else {
+
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+
+							} for (itThree; itThree != newVector.rend(); itThree++) {
+
+								addition = *itThree;
+
+								if (tenPlus == true) {
+
+									addition += 1;
+									tenPlus = false;
+								}
+
+								if (addition >= 10) {
+
+									tenPlus = true;
+									addition -= 10;
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+								else {
+
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+							}
+
+						}
+						else {
+
+							for (itThree; itThree != bigVector.rend(); itThree++, itFour++) {
+
+								addition = *itThree + *itFour;
+
+								if (tenPlus == true) {
+
+									addition += 1;
+									tenPlus = false;
+								}
+
+								if (addition >= 10) {
+
+									tenPlus = true;
+									addition -= 10;
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+								else {
+
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+
+							} for (itFour; itFour != firstVector.rend(); itFour++) {
+
+								addition = *itFour;
+
+								if (tenPlus == true) {
+
+									addition += 1;
+									tenPlus = false;
+								}
+
+								if (addition >= 10) {
+
+									tenPlus = true;
+									addition -= 10;
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+								else {
+
+									newVector.insert(newVector.begin(), addition);
+									addition = 0;
+
+								}
+
+							}
+
+							if (tenPlus == true) {
+
+								newVector.insert(newVector.begin(), 1);
+							}
+
+						}
+
+						firstVector.clear();
+
+						for (int i = 0; i < multiplicationCounter; i++) {
+
+							
+							firstVector.push_back(0);
+
+						}
 						for (it; it != bigVector.rend(); it++) {
 
-							multiplication = *it * *itTwo;
-							multiplication += carry;
-							carry = 0;
+							if (tenPlus == true) {
 
+								multiplication = *it * *itTwo;
+								multiplication += carry;
+								carry = 0;
+								tenPlus = false;
+
+							}
+							else {
+
+								multiplication = *it * *itTwo;
+
+							}
 							if (multiplication >= 10) {
 
 								carry = multiplication / 10;
 								multiplication = multiplication / (carry * 10);
 								tenPlus = true;
+								firstVector.insert(firstVector.begin(), multiplication);
+								
 
+							}
+							else {
+
+								firstVector.insert(firstVector.begin(), multiplication);
 
 							}
 							
@@ -337,18 +486,71 @@ namespace BigNumbers
 						}
 					}
 
+				} else {
+
+				for (itTwo; itTwo != vectorTwo.bigVector.rend(); itTwo++, multiplicationCounter++) {
+
+				
+		
+					firstVector.clear();
+
+					for (int i = 0; i < multiplicationCounter; i++) {
+
+
+						firstVector.push_back(0);
+
+					}
+					for (it; it != bigVector.rend(); it++) {
+					  
+						if (tenPlus == true) {
+
+							multiplication = *it * *itTwo;
+							multiplication += carry;
+							carry = 0;
+							tenPlus = false
+
+						}
+						else {
+
+							multiplication = *it * *itTwo;
+
+						}
+
+						
+
+						if (multiplication >= 10) {
+
+							carry = multiplication / 10;
+							multiplication = multiplication / (carry * 10);
+							tenPlus = true;
+							firstVector.insert(firstVector.begin(), multiplication);
+
+
+						}
+						else {
+
+							firstVector.insert(firstVector.begin(), multiplication);
+
+						}
+
+
+
+					}
 				}
 
 
 
 
+			}
+
+			return newVector;
 			
 			} */
-			std::vector<int>& operator /(MyBigNumbers vectorTwo) {} 
+            std::vector<int>& operator /(MyBigNumbers vectorTwo) {}
 			std::vector<int>& operator %(MyBigNumbers vectorTwo) {}
 			
 			
 		 }; 
 
 
-	}
+    } 
